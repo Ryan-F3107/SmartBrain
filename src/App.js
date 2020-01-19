1c8ff6a7bf7dc856f727bac486c91a7af255a76c
Ryan-F3107 <47283124+Ryan-F3107@users.noreply.github.com>
2020-01-18 19:10:45 -0500

    move files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/*
+      <Logo />
+      <ImageLinkForm />
+      <FaceRecognition />
+      */}
+    </div>
+  );
+}
+
+export default App;
