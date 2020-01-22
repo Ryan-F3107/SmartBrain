45eecf4792166bacae8b5b6cc41a2aec1cdcfc63
Ryan-F3107 <47283124+Ryan-F3107@users.noreply.github.com>
2020-01-21 21:31:04 -0500

    Front-end,Pt1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import './App.css';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const particlesOption = {
+  particles: {
+    number: {
+      value: 250,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' 
+        params={particlesOption}
+      />
       <Navigation />
-      {/*
       <Logo />
+      <Rank />
       <ImageLinkForm />
+      {/*
       <FaceRecognition />
       */}
     </div>
