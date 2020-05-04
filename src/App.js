2f91f85f0be8ea92def615a04be734e201e37540
Ryan-F3107 <47283124+Ryan-F3107@users.noreply.github.com>
2020-05-04 19:55:41 -0400

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -86,32 +86,33 @@ class App extends Component {
 
   onSubmitClick = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageUrl', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        input: this.state.input
+      fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
       })
-    })  //end of fetch block
-    .then(response => response.json())
-    .then(response => {
-        if(response){
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
           fetch('http://localhost:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
               id: this.state.user.id
             })
-          })  //end of fetch block
-          .then(response => response.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, {entries: count}))
           })
-          .catch(console.log) //improves error handling
-        } //end of if statement 
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+            .catch(console.log)
+
+        } //end of id statement
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
-      .catch(err => console.log(err)); //catch works like an else statement 
+      .catch(err => console.log(err)); 
   } //end of onSubmitClick
 
   onRouteChange = (route) => {
