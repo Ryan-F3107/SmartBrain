85311ff319d63512416675dcec86c30a92fcbf32
Ryan-F3107 <47283124+Ryan-F3107@users.noreply.github.com>
2020-05-10 17:13:13 -0400

    worked on bugs

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://quiet-waters-20582.herokuapp.com:3000/image', {
+          fetch('https://quiet-waters-20582.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
